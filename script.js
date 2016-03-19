cdf31d5056b16375c6b88f32ada33c919a8df9ab
Dijin <dijin@lookup.to>
2016-03-19 17:04:34 +0530

    Moved code to self executing function

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-var people = [];
-var template = $('#peopleTemplate').html();
-console.log(template);
-// .html() jquery func
-$('#peopleModule').find('button').on('click', function () {
-    "use strict";
-    people.push($('#peopleModule').find('input').val());
-    $('#peopleModule').find('input').val('');
-    //.val() jquery func
-    var data = {
-        people: people
-    };
-    var html = Mustache.to_html(template, data);
-    $('#peopleModule').find('ul').html(Mustache.to_html(template, data));
-});
+/* To avoid using global variables
 
-$('#peopleModule').find('ul').delegate('div.del', 'click', function (e) {
+we can wrap entire module into a function
+
+(function(){
+var template = $('#peopleTemplate').html();
+})()
+
+ */
+
+(function(){
     "use strict";
-    var $remove = $(e.target).closest('li');
-    //closest jQuey
-    var i = $('#peopleModule').find('ul').find('li').index($remove);
-    $remove.remove();
-    people.slice(i, 1);
-});
+    var people = [];
+    var template = $('#peopleTemplate').html();
+// to avoid writing html code inside js we are fetching it
+    console.log(template);
+// .html() jquery func
+    $('#peopleModule').find('button').on('click', function () {
+        "use strict";
+        people.push($('#peopleModule').find('input').val());
+        $('#peopleModule').find('input').val('');
+        //.val() jquery func
+        var data = {
+            people: people
+        };
+        var html = Mustache.to_html(template, data);
+        $('#peopleModule').find('ul').html(Mustache.to_html(template, data));
+    });
+
+    $('#peopleModule').find('ul').delegate('div.del', 'click', function (e) {
+        "use strict";
+        var $remove = $(e.target).closest('li');
+        //closest jQuey
+        var i = $('#peopleModule').find('ul').find('li').index($remove);
+        $remove.remove();
+        people.slice(i, 1);
+    });
+})();
+
+
