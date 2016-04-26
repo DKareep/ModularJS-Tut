190cf55c883d138615f8001a0d309c4517ad4d5d
Dijin <dijin@lookup.to>
2016-04-26 22:02:04 +0530

    protype inheritance

--- script6.js
+++ script6.js
@@ -1,8 +1,54 @@
 // instantiation -- classical prototype
 
-var Person = function() {
+var Person = function (name) {
     "use strict";
-
+    this.name = name;
 };
 
-var john = new Person();
+function inherits(ctor, superCtor) {
+    "use strict";
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+     constructor: {
+         value: ctor,
+         enumerable: false,
+         writable: true,
+         configurable: true
+     }
+  });
+};
+
+// get created on Person automatically
+Person.prototype = {};
+
+Person.prototype.sayName = function() {
+    "use strict";
+  console.log("Hi i am", this.name);
+};
+
+Person.prototype.shoutName = function () {
+    console.log('Hey !! i am ' + this.name);
+};
+
+var john = new Person('john');
+
+john.sayName();
+
+var Musician = function(name, instrument) {
+    "use strict";
+    Musician.super_.call(this,name);
+    this.instrument = instrument;
+};
+
+inherits(Musician,Person);
+Musician.prototype.play = function() {
+    "use strict";
+    console.log(this.instrument);
+};
+//Musician.prototype.shoutName = function () {
+//    console.log('Hey !! i am ' + this.name + '!!!');
+//};
+var MJ = new Musician("MJ", "Flute");
+MJ.sayName();
+MJ.play();
+MJ.shoutName();
